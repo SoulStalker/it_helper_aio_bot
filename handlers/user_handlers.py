8ed650210eedb7c23d36c1c8c3b6f36e863a7331
Almaz Galimov <almazwork@gmail.com>
2024-04-21 22:29:57 +0500

    add handlers for main commands start, help, contacts, service

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -11,6 +11,25 @@ from lexicon.lexicon import LEXICON_RU
 router = Router()
 
 
-@router.message(CommandStart)
-async def send_message(message: Message):
+# Этот хендлер срабатывает на команду /start
+@router.message(CommandStart())
+async def start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'])
+
+
+# Этот хендлер срабатывает на команду /help
+@router.message(Command('help'))
+async def help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
+
+
+# Этот хендлер срабатывает на команду /contacts
+@router.message(Command('contacts'))
+async def contacts_command(message: Message):
+    await message.answer(text=LEXICON_RU['/contacts'])
+
+
+# Этот хендлер срабатывает на команду /service
+@router.message(Command('service'))
+async def service_command(message: Message):
+    await message.answer(text=LEXICON_RU['/service'])
